523492826ed6bdbb6904bb76f974b0aec8b0c418
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-09-30 16:03:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,8 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
-    # Specify the AzureRM provider
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0" # Renovate will target this version constraint
+      version = "=3.84.0"
     }
   }
-}
-
-# The required features block for azurerm provider
-provider "azurerm" {
-  features {}
-}
-
-# Example resource
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-renovate-test"
-  location = "East US"
 }
